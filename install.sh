edb864428ec5aa7c563aa0089e4a92b976e750e6
Chenhao Ye <ye.chenhao.ych@gmail.com>
2020-12-23 12:29:53 -0600

    fix sed

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ if ! [ -d ~/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 cp thyme.zsh-theme $ZSH_CUSTOM/themes/
-sed 's/\(^ZSH_THEME=.*$\)/ZSH_THEME="thyme"/g' ~/.zshrc
+sed -i 's/\(^ZSH_THEME=.*$\)/ZSH_THEME="thyme"/g' ~/.zshrc
