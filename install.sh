0d09ee1b22616fe31a4fe030669506bd571a6c19
Chenhao Ye <ye.chenhao.ych@gmail.com>
2021-11-26 11:24:02 -0600

    add install success message

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-
+set -e
 if ! command -v starship >/dev/null 2>&1 ; then
     # if brew is available, use brew
     if command -v starship >/dev/null 2>&1 ; then
@@ -13,3 +13,5 @@ mkdir -p "$HOME/.config/starship/"
 cp thyme.toml "$HOME/.config/starship/thyme.toml"
 echo 'export STARSHIP_CONFIG="$HOME/.config/starship/thyme.toml"' >> $HOME/.zshrc
 echo 'eval "$(starship init zsh)"' >> $HOME/.zshrc
+
+echo "Thyme is installed successfully!"
