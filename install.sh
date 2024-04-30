b8b0b242170354e4ac9412956f3825a91dc93a4e
Chenhao Ye <ye.chenhao.ych@gmail.com>
2024-04-29 21:55:28 -0500

    Use `sudo` to install starship

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if ! command -v starship >/dev/null 2>&1 ; then
     if command -v starship >/dev/null 2>&1 ; then
         brew install starship
     else
-        curl -sS https://starship.rs/install.sh | sh
+        curl -sS https://starship.rs/install.sh | sudo sh
     fi
 fi
 
